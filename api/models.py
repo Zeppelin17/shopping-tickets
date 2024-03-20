28248e19d7fda6d86e26f6f4b267fbc0615d3835
Zeppelin17 <polmonclus@outlook.com>
2024-03-20 21:59:53 +0100

    feat: add models __str__ function

--- api/models.py
+++ api/models.py
@@ -2,10 +2,18 @@ from django.db import models
 
 # Create your models here.
 
+
 class Shop(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 
 class Ticket(models.Model):
     date = models.DateField()
     shop = models.ForeignKey(Shop, null=True, on_delete=models.SET_NULL)
     amount = models.FloatField()
+
+    def __str__(self):
+        return f"{self.date} - {self.shop}"
