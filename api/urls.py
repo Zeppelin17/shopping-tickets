e503728e2b47565eda8a5875c96fbb6d483b7836
Zeppelin17 <polmonclus@outlook.com>
2024-04-07 12:37:24 +0200

    feat: add query params options when getting tickets from API

--- api/urls.py
+++ api/urls.py
@@ -3,16 +3,16 @@ from rest_framework import routers
 from api import views
 
 from knox import views as knox_views
-from .views import  LoginAPI, RegisterAPI
+from .views import LoginAPI, RegisterAPI
 
 router = routers.DefaultRouter()
-router.register(r'tickets', views.TicketViewSet)
-router.register(r'shops', views.ShopViewSet)
+router.register(r"tickets", views.TicketViewSet, basename="tickets")
+router.register(r"shops", views.ShopViewSet)
 
 urlpatterns = [
-    path('signup', RegisterAPI.as_view(), name='Sign Up'),
-    path('login', LoginAPI.as_view(), name='Login'),
-    path('logout', knox_views.LogoutView.as_view(), name='Logout'),
-    path('logoutall', knox_views.LogoutAllView.as_view(), name='Logout All'),
-    path('', include(router.urls)),
+    path("signup", RegisterAPI.as_view(), name="Sign Up"),
+    path("login", LoginAPI.as_view(), name="Login"),
+    path("logout", knox_views.LogoutView.as_view(), name="Logout"),
+    path("logoutall", knox_views.LogoutAllView.as_view(), name="Logout All"),
+    path("", include(router.urls)),
 ]
--- api/views.py
+++ api/views.py
@@ -1,21 +1,53 @@
 from rest_framework import viewsets, generics, permissions
 from rest_framework.response import Response
-from .serializer import TicketSerializer, ShopSerializer, RegisterSerializer, UserSerializer
+from .serializer import (
+    TicketSerializer,
+    ShopSerializer,
+    RegisterSerializer,
+    UserSerializer,
+)
 from django.contrib.auth import login
 from rest_framework import permissions
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from knox.views import LoginView as KnoxLoginView
 from knox.models import AuthToken
 from .models import Ticket, Shop
+from datetime import datetime
+
 
 # Create your views here.
 class ShopViewSet(viewsets.ModelViewSet):
     queryset = Shop.objects.all()
     serializer_class = ShopSerializer
 
+
 class TicketViewSet(viewsets.ModelViewSet):
-    queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
+
+    def get_queryset(self):
+        """
+        A function that retrieves a queryset based on the query parameters.
+
+        Parameters:
+            - without query params: returns all Ticket objects
+            - current_month: returns all Ticket objects for the current month.
+                - possible values: [0, 1]
+
+        Returns:
+            queryset: a queryset of Ticket objects based on the request parameters
+        """
+        try:
+            only_current_month = int(self.request.query_params.get("current_month", 0))
+        except ValueError:
+            only_current_month = 0
+
+        if only_current_month == 1:
+            queryset = Ticket.objects.filter(date__month=datetime.now().month)
+        else:
+            queryset = Ticket.objects.all()
+
+        return queryset
+
 
 class RegisterAPI(generics.GenericAPIView):
     serializer_class = RegisterSerializer
@@ -24,10 +56,15 @@ class RegisterAPI(generics.GenericAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
-        return Response({
-            "user": UserSerializer(user, context=self.get_serializer_context()).data,
-            "token": AuthToken.objects.create(user)[1]
-        })
+        return Response(
+            {
+                "user": UserSerializer(
+                    user, context=self.get_serializer_context()
+                ).data,
+                "token": AuthToken.objects.create(user)[1],
+            }
+        )
+
 
 class LoginAPI(KnoxLoginView):
     permission_classes = (permissions.AllowAny,)
@@ -35,6 +72,6 @@ class LoginAPI(KnoxLoginView):
     def post(self, request, format=None):
         serializer = AuthTokenSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
+        user = serializer.validated_data["user"]
         login(request, user)
         return super(LoginAPI, self).post(request, format=None)
